f9a2fdb57c1181b3d470a13c66eea00ebfa8c791
asa121asa <crafta.net@gmail.com>
2020-11-19 16:39:54 +0300

    upd

--- src/redux/state.tsx
+++ src/redux/state.tsx
@@ -31,11 +31,80 @@ export type stateType = {
     dialogsPage: messagePageType
     sidebar: {}
 }
-let rerenderEntireTree = () =>{}
+/*let rerenderEntireTree = () =>{}*/
+
+export type StroreType ={
+    _state:stateType
+    addPost:(postText: string)=>void
+    addMessage:(postMessage: string)=>void
+    rerenderEntireTree:()=>void
+    subscribe: (observer:()=>void)=>void
+}
+
+const Store: StroreType ={
+    _state:  {
+        profilePage: {
+            messageForNewPost: "",
+            posts: [
+                {id: 1, message: 'Hi, how are you*?', likekounts: 12},
+                {id: 2, message: 'its my first post', likekounts: 33},
+                {id: 3, message: 'go in iron', likekounts: 333},
+                {id: 3, message: 'its my life', likekounts: 333}
+            ],
+        },
+        dialogsPage: {
+            dialogs: [
+                {id: 1, name: "Nastya"},
+                {id: 2, name: "Vlad"},
+                {id: 3, name: "Vika"},
+                {id: 4, name: "Andrey"},
+                {id: 5, name: "Vasia"}
+            ],
+
+            messagesForMessages: "",
+            messages: [
+                {id: 1, message: "hi how is your programmnig"},
+                {id: 2, message: "hi, its ok\""},
+                {id: 3, message: "Good"},
+                {id: 4, message: "ok"},
+                {id: 5, message: "yo"}
+            ]
+        },
+        sidebar: {}
+    },
+    addPost(postText: string)  {
+        debugger
+        this._state.profilePage.messageForNewPost = postText
+        let newPost: PostsTypes = {
+            id: 6,
+            likekounts: 0,
+            message: this._state.profilePage.messageForNewPost
+        }
+
+        this._state.profilePage.posts.push(newPost)
+
+        this.rerenderEntireTree()
+    },
+    addMessage(postMessage: string)  {
+        debugger
+        this._state.dialogsPage.messagesForMessages = postMessage
+        let newMessage: MessagesType = {
+            id: 6,
+            message: this._state.dialogsPage.messagesForMessages
+        }
+
+        this._state.dialogsPage.messages.push(newMessage)
+        this.rerenderEntireTree()
+    },
+    rerenderEntireTree() {console.log('State Changed')},
+    subscribe(observer:()=>void){
+        this.rerenderEntireTree=observer
+    }
 
 
+}
 
-const state: stateType = {
+/*const state: stateType = {
     profilePage: {
         messageForNewPost: "",
         posts: [
@@ -64,10 +133,10 @@ const state: stateType = {
         ]
     },
     sidebar: {}
-}
+}*/
 
 
-export let addPost = (postText: string) => {
+/*export let addPost = (postText: string) => {
     debugger
     state.profilePage.messageForNewPost = postText
     let newPost: PostsTypes = {
@@ -79,12 +148,12 @@ export let addPost = (postText: string) => {
     state.profilePage.posts.push(newPost)
 
     rerenderEntireTree()
-}
+}*/
 
 
 
 
-export let addMessage = (postMessage: string) => {
+/*export let addMessage = (postMessage: string) => {
     debugger
     state.dialogsPage.messagesForMessages = postMessage
     let newMessage: MessagesType = {
@@ -94,16 +163,18 @@ export let addMessage = (postMessage: string) => {
 
     state.dialogsPage.messages.push(newMessage)
     rerenderEntireTree()
-}
+}*/
 
 
 
+/*
 export const subscribe = (observer:()=>void)=>{
     rerenderEntireTree=observer
 
 }
+*/
 
 
 
-export default state
+export default Store
 
